cf6a4855944d360efbd86ec23ef93237328f0352
jzhang172 <jzhang172@gmail.com>
2015-09-11 23:06:51 -0400

    Checking if anything changes when folder is moved | No code change

--- custom.js
+++ custom.js
@@ -1,12 +1,14 @@
 
 $(function(){
 	$("p.hideme").hide();
-
-// -=-=-=-=-=Minisect Toggling -=-=-=-=-=-=-= //
+    $("p.hideme2").hide();
+    $("p.hideme3").hide();
+// -=-=-=-=-=Minisect Toggling: Description -=-=-=-=-=-=-= //
 	$(".minisect.description").click(function(){
-		$("p.hideme").fadeToggle(500);
-
+		
+      $("p.hideme").css("background", "rgb(241, 241, 241)");
 		var element= document.querySelector(".minisect.description").offsetHeight;
+		
 		if(element==30)
 		{
 			$(".minisect.description").animate({height:"300px"},500);
@@ -14,7 +16,36 @@ $(function(){
 		else{
 			$(".minisect.description").animate({height:"30px"},500);
 		}
+		$("p.hideme").delay(600).fadeToggle(600);
 	});
+// -=-=-=-=-=Minisect Toggling: Background -=-=-=-=-=-=-= //
+	$(".minisect.background").click(function(){
+		$("p.hideme2").fadeToggle(900);
+
+		var element= document.querySelector(".minisect.background").offsetHeight;
+		$("p.hideme2").css("background", "rgb(241, 241, 241)");
+		if(element==30)
+		{
+			$(".minisect.background").animate({height:"400px"},500);
+		}
+		else{
+			$(".minisect.background").animate({height:"30px"},500);
+		}
+	});
+// -=-=-=-=-=Minisect Toggling: Powers -=-=-=-=-=-=-= //
+	$(".minisect.powers").click(function(){
+		$("p.hideme3").fadeToggle(900);
+        
+		var element= document.querySelector(".minisect.powers").offsetHeight;
+		$("p.hideme3").css("background", "rgb(241, 241, 241)");
+		if(element==30)
+		{
+			$(".minisect.powers").animate({height:"500px"},500);
+		}
+		else{
+			$(".minisect.powers").animate({height:"30px"},500);
+		}
+	});	
 // -=-=-=-=-=Background Toggling -=-=-=-=-=-=-= //
 
 
